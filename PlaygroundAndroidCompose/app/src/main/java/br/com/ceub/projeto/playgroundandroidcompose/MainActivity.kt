63aa639319657a4a0b1d34fa23504f848a19ac9e
Fernando Dias <fernando.dias@sicoob.com.br>
2024-04-08 10:45:10 -0300

    feat(*): Aula introdução ao Android Compose

--- PlaygroundAndroidCompose/app/src/main/java/br/com/ceub/projeto/playgroundandroidcompose/MainActivity.kt
+++ PlaygroundAndroidCompose/app/src/main/java/br/com/ceub/projeto/playgroundandroidcompose/MainActivity.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.ceub.projeto.playgroundandroidcompose.ui.theme.PlaygroundAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +44,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    Cabecalho(nome = "Fernando", sobreNome = "Dias")
                 }
             }
         }
@@ -58,7 +72,7 @@ class MainActivity : ComponentActivity() {
     override fun onStop() {
         super.onStop()
 
-        Log.i("TELA", "ACESSANDO onPause")
+        Log.i("TELA", "ACESSANDO onStop")
     }
 
 
@@ -69,15 +83,103 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Magenta) {
         Text(
             text = "Olá Mundo  $name!",
+            fontSize = 60.sp,
+            lineHeight = 50.sp,
             modifier = modifier.padding(32.dp)
         )
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PlaygroundAndroidComposeTheme {
-        Greeting(name = "Fernando Dias")
+fun ItemCard(valor: String) {
+    Surface(
+        modifier = Modifier
+            .width(80.dp)
+            .height(40.dp), color = Color.DarkGray
+    ) {
+        Box {
+            Text(
+                text = valor,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
     }
 }
+
+@Composable
+fun Cabecalho(nome: String, sobreNome: String) {
+    //Organização de Layout
+    // Row, Columns
+
+
+    Surface {
+        Box(modifier = Modifier) {
+            Image(
+                painter = painterResource(id = R.drawable.background_app),
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+            )
+
+            Column {
+                Row(modifier = Modifier.padding(16.dp)) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_user),
+                        contentDescription = "Ícone do usuário",
+                        modifier = Modifier
+                            .size(60.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                    Column(modifier = Modifier.padding(32.dp)) {
+                        Text(
+                            text = nome,
+                            fontSize = 40.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = sobreNome,
+                            fontSize = 12.sp,
+                            modifier = Modifier.align(Alignment.End)
+                        )
+                    }
+                }
+
+                /*
+                        Crie  uma linha com e Elementos
+                         - Adicione na row horizontalArrangement
+                         - Faça testes com Arrament.*
+                         - Adicione na row modifier = Modifier.fillMaxSize()
+                     */
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    ItemCard(valor = "Confirmar")
+                    ItemCard(valor = "Cancelar")
+                }
+
+
+            }
+        }
+    }
+}
+
+
+@Preview(name = "Preview Cabeçalho")
+@Composable
+fun CabecalhoPreview() {
+    PlaygroundAndroidComposeTheme {
+        Cabecalho(nome = "Fernando", sobreNome = "Dias")
+    }
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    PlaygroundAndroidComposeTheme {
+//        Greeting(name = "Fernando Dias")
+//    }
+//}
